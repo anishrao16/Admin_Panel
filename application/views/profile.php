6fbc74ed4e50a8da1db26640c774b40b1465c56f
Aneesh <rao.anish63@gmail.com>
2020-02-04 16:19:14 +0530

    update

--- application/views/profile.php
+++ application/views/profile.php
@@ -22,6 +22,70 @@ $role = $userInfo->role;
                     <div class="box-body box-profile">
                         <img class="profile-user-img img-responsive img-circle" src="<?php echo base_url(); ?>assets/dist/img/avtar.png" alt="User Profile Picture">
                         <h3 class="profile-username text-center"> <?= $name ?> </h3>
+                        <p class="text-muted text-center"> <?= $role ?> </p>
+
+                        <ul class="list-group list-group-unbordered">
+
+                            <li class="list-group-item">
+                                <b>Email</b> <a class="pull-right"> <?= $email ?> </a>
+                            </li>
+
+                            <li class="list-group-item">
+                                <b>Mobile</b> <a class="pull-right"> <?= $mobile ?> </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-5">
+                <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs">
+                        <li class="<?= ($active == "details")? "active" : "" ?>"> <a href="#details" data-toggle="tab"> Details </a>  </li>
+                        <li class="<?= ($active == "changepass")? "active" : "" ?>"><a href="#changepass" data-toggle="tab">Change Password</a></li>
+                    </ul>
+
+                    <div class="tab-content">
+                        <div class="<?= ($active ==  "deatils")? "active" : "" ?> tab-pane" id="details">
+                            <form action="<?php echo base_url(); ?>profileUpdate" method="post" id="editProfile" role="form">
+                                <?php $this->load->helper('form'); ?>
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="fname"> Full Name </label>
+                                                <input type="text" class="form-control" id="fname" name="fname" placeholder="<?php echo $name; ?>" value="<?php echo set_value('fname', $name); ?>" />
+                                                <input type="hidden" value="<?php echo $userId; ?>" name="userId" id="userId" />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="mobile"> Mobile Number </label>
+                                                <input type="tel" class="form-control" id="mobile" name="mobile" placeholder="<?php echo $mobile; ?>" value="<?php echo set_value('mobile', $mobile); ?>" />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="email"> Email Address </label>
+                                                <input type="email" class="form-control" id="email" name="email" placeholder="<?php echo $email; ?>" value="<?php echo set_value('email', $email); ?>" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="box-footer">
+                                    <input type="submit" class="btn btn-primary" value="Submit" />
+                                    <input type="reset" class="btn btn-default" value="Reset" />
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
