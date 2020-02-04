43908682e8311b4ba6cc9859d220d59b970371d2
Aneesh <rao.anish63@gmail.com>
2020-02-04 15:44:24 +0530

    update profile

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,69 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title> My Profile </title>
-        <?php echo link_tag('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>
-        <?php echo link_tag('assets/vendor/fontawesome-free/css/all.min.css'); ?>
-        <?php echo link_tag('assets/vendor/datatables/dataTables.bootstrap4.css'); ?> 
-    </head>
+<?php
+$userId = $userInfo->userId;
+$name = $userInfo->name;
+$email = $userInfo->email;
+$mobile = $userInfo->mobile;
+$roleId = $userInfo->roleId;
+$role = $userInfo->role;
+?>
 
-    <body id="page-top">
-        <?php include APPPATH.'views/user/includes/header.php'; ?>
+<div class="content-wrapper">
+    <sectio class="content-header">
+        <h1>
+            <i class="fa fa-user-circle"> </i> My Profile
+            <small> View or Modify the information </small>
+        </h1>
+    </sectio>
 
-        <div id="wrapper">
-            <?php include APPPATH.'views/user/includes/sidebar.php'; ?>
-            <div id="content-wrapper">
-                <div class="container-fluid">
-                    
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">
-                            <a href="<?php echo site_url('user/Dashboard'); ?>"> User </a>
-                        </li>
-                        
-                        <li class="breadcrumb-item active"> My Profile </li>
-                    </ol>
-
-                    <h1> My Profile </h1>
-                    <hr>
-
-                    <?php if($this->session->flashdata('success')) { ?>
-                        <p style="color: green; font-size: 18px;"> <?php echo $this->session->flashdata('success'); ?> </p>
-                        </div>
-                    <?php } ?>
-
-                    <?php if($this->session->flashdata('error')) {?>
-                        <p style="color: red; font-size: 18px;"> <?php echo $this->session->flashdata('error'); ?> </p> 
-                    <?php } ?>
-
-                    <?php echo form_open('user/User_profile/updateprofile'); ?>
-
-                    <p> <strong> Reg. Date : </strong> <?php echo $profile->regDate; ?> </p>
-
-                    <div class="form-group">
-                        <div class="form-row">
-                            <div class="col-md-6">
-                                <div class="form-label-group">
-                                    <?php echo form_input(['name'=>'firstname','id'=>'firstname','class'=>'form-control','autofocus'=>'autofocus','value'=>set_value('firstname',$profile->firstname)]); ?>
-                                    <?php echo form_label('Enter your first name','firstname'); ?>
-                                    <?php echo form_error('firstname',"<div style='color:red'> "," </div>"); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="form-row">
-                            <div class="col-md-6">
-                                <div class="form-label-group">
-                                    <?php echo form_input(['name'=>'lastname','id'=>'lastname','class'=>'form-control','autofocus'=>'autofocus','value'=>set_value('lastname',$profile->lastname)]); ?>
-                                    <?php echo form_label('Enter your last name','lastname'); ?>
-                                    <?php echo form_error('lastname',"<div style='color:red'> "," </div>"); ?>
-                                </div>
-                            </div>
-                        </div>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="box box-warning">
+                    <div class="box-body box-profile">
+                        <img class="profile-user-img img-responsive img-circle" src="<?php echo base_url(); ?>assets/dist/img/avtar.png" alt="User Profile Picture">
+                        <h3 class="profile-username text-center"> <?= $name ?> </h3>
                     </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </section>
+</div>
