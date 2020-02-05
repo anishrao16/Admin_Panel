4becd03c30e3b93f0b83b772081540d23ec26d38
Aneesh <rao.anish63@gmail.com>
2020-02-05 12:41:14 +0530

    update login history

--- application/views/loginHistory.php
+++ application/views/loginHistory.php
@@ -20,10 +20,64 @@
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 form-group">
                     <div class="input-group">
                         <input id="toDate" type="text" name="toDate" value="<?php echo $toDate; ?>" class="form-control datepicker" placeholder="To Date" autocomplete="off" />
-                        <span class="input-group-addon">  </span>
+                        <span class="input-group-addon"> <label for="toDate"> <i class="fa fa-calender" /> </label> </span>
                     </div>
                 </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 form-group">
+                    <input id="searchText" type="text" name="searchText" value="<?php echo $searchText; ?>" class="form-control" placeholder="Search Text" />
+                </div>
+
+                <div class="col-lg-1 col-md-1 col-sm-6 col-xs-6 form-group">
+                    <button type="submit" class="btn btn-primary btn-md btn-block searchList pull-right"> <i class="fa fa-search" aria-hidden="true" /> </button>
+                </div>
+
+                <div class="col-lg-1 col-md-1 col-sm-6 col-xs-6 form-group">
+                    <button class="btn btn-md btn-default btn-block pull-right resetFilters"> <i class="fa fa-refresh" aria-hidden="true" /> </button>
+                </div>
+
             </form>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"> <?= !empty($userInfo) ? $userInfo->name." : ".$userInfo->email : "All Users" ?> </h3>
+                        <div class="box-tools"></div>
+                    </div>
+
+                    <div class="box-body table-responsive no-padding">
+                        <table class="table table-hover">
+                            <tr>
+                                <th> Session Data </th>
+                                <th> IP Address </th>
+                                <th> User Agent </th>
+                                <th> Agent Full String </th>
+                                <th> Platform </th>
+                                <th> Date-Time </th>
+                            </tr>
+
+                            <?php
+                            if(!empty($userRecords))
+                            {
+                                foreach($userRecords as $record)
+                                {
+                                    ?>
+                                    <tr>
+                                        <td> <?php echo $record->sessionData ?> </td>
+                                        <td> <?php echo $record->sessionData ?> </td>
+                                        <td> <?php echo $record->sessionData ?> </td>
+                                        <td> <?php echo $record->sessionData ?> </td>
+                                        <td> <?php echo $record->sessionData ?> </td>
+                                        <td> <?php echo $record->sessionData ?> </td>
+                                    </tr>
+                                }
+                            }?>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </div>
