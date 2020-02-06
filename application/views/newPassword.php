f8f0427b41f6fc600d68215068899e988d704a7c
Aneesh <rao.anish63@gmail.com>
2020-02-06 11:16:37 +0530

    new password

---
+++ application/views/newPassword.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Reset Password </title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <link rel="stylesheet" href="<?php echo base_ur(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo base_ur(); ?>assets/bower_components/font-awesome/css/font-awesome.min.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo base_ur(); ?>assets/dist/css/AdminLTE.min.css" type="text/css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    </head>
+
+    <body class="login-page">
+        <div class="login-box">
+            <div class="login-logo">
+                <a href="#"> <b> Admin Panel </b> </a>
+            </div>
