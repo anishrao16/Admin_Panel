662e718db671a353df5dcfbd9911a3d6b77aada6
Aneesh <rao.anish63@gmail.com>
2020-02-06 17:17:18 +0530

    update

--- application/helpers/admin_helper.php
+++ application/helpers/admin_helper.php
@@ -77,4 +77,38 @@
         }
     }
 
+    if(!function_exists('emailConfig'))
+    {
+        function emailConfig()
+        {
+            $CI->load->library('email');
+            $config['protocol'] = PROTOCOL;
+            $config['smtp_host'] = SMTP_HOST;
+            $config['smtp_port'] = SMTP_PORT;
+            $config['mailpath'] = MAIL_PATH;
+            $config['charset'] = 'UTF-8';
+            $config['mailtype'] = "html";
+            $config['newline'] = "\r\n";
+            $config['wordwrap'] = TRUE;
+        }
+    }
+
+    if(!function_exists('resetPasswordEmail'))
+    {
+        function resetPasswordEmail($detail)
+        {
+            $data["data"] = $detail;
+
+            $CI = setProtocol();
+
+            $CI->email->from(EMAIL_FROM, FROM_NAME);
+            $CI->email->subject("Reset Password");
+            $CI->email->message($CI->load->view('email/resetPassword', $data, TRUE));
+            $CI->email->to($detail["email"]);
+            $status = $CI->email->send();
+
+            return $status;
+        }
+    }
+
 ?>
