c95a671a2a050016bda791fc702da20e19ad642a
Aneesh <rao.anish63@gmail.com>
2020-02-07 15:37:49 +0530

    created login model

---
+++ application/models/Login_model.php
@@ -0,0 +1,112 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login_model extends CI_Model
+{
+    function loginMe($email, $password)
+    {
+        $this->db->select('BaseTbl.userId, BaseTbl.password, BaseTbl.name, BaseTbl.roleId, Roles.role');
+        $this->db->from('tbl_users as BaseTbl');
+        $this->db->join('tbl_roles as Roles','Roles.roleId = BaseTbl.roleId');
+        $this->db->where('BaseTbl.email', $email);
+        $this->db->where('BaseTbl.isDeleted', 0);
+        $query = $this->db->get();
+
+        $user = $query->row();
+
+        if(!empty($user))
+        {
+            if(verifyHashedPassword($password, $user->password))
+            {
+                return $user;
+            }
+            else
+            {
+                return array();
+            }
+        }
+        else
+        {
+            return array();
+        }
+    }
+
+    function checkEmailExists($email)
+    {
+        $this->db->select('userId');
+        $this->db->where('email', $email);
+        $this->db->where('isDeleted', 0);
+        $query = $this->db->get('tbl_users');
+
+        if($query->num_rows() > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    function resetPasswordUser($data)
+    {
+        $result = $this->db->insert('tbl_reset_password', $data);
+
+        if($result)
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    function getCustomerInfoByEmail($email)
+    {
+        $this->db->select('userId, email, name');
+        $this->db->from('tbl_users');
+        $this->db->where('isDeleted', 0);
+        $this->db->where('email', $email);
+        $query = $this->db->get();
+
+        return $query->row();
+    }
+
+    function checkActivationDetails($email, $activation_id)
+    {
+        $this->db->select('id');
+        $this->db->from('tbl_reset_password');
+        $this->db->where('email', $email);
+        $this->db->where('activation_id', $activation_id);
+        $query = $this->db->get();
+
+        return $query->num_row();
+    }
+
+    function createPasswordUser($email, $password)
+    {
+        $this->db->where('email', $email);
+        $this->db->where('isDeleted', 0);
+        $this->db->update('tbl_users', array('password'=>getHashedPassword($password)));
+        $this->db->delete('tbl_reset_password', array('email'=>$email));
+    }
+
+    function lastLogin($loginInfo)
+    {
+        $this->db->trans_start();
+        $this->db->insert('tbl_last_login', $loginInfo);
+        $this->db->trans_complete();
+    }
+
+    function lastLoginInfo($userData)
+    {
+        $this->db->select('BaseTbl.createdDtm');
+        $this->db->where('BaseTbl.userId', $userId);
+        $this->db->order_by('BaseTbl.id', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get('tbl_last_login as BaseTbl');
+
+        return $query->row();
+    }
+}
+?>
